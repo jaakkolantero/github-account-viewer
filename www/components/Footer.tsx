cff54367a4189910a28fb04c0b601fc8e6c4808b
UFo_x <tero.jaakkola1@gmail.com>
2019-12-10 20:13:38 +0200

    style(footer): fine tune footer text

--- www/components/Footer.tsx
+++ www/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="flex justify-center w-full bg-black text-gray-300 py-8 px-4">
       <a
-        className="text-gray-400 hover:text-white"
+        className="text-gray-400 hover:text-white tracking-tighter font-bold"
         href="https://github.com/jaakkolantero"
       >
         @jaakkolantero
